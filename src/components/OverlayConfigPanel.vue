f70c1a1b89a7e3ce807bb45841ae99b9cf35b2cf
longhai_shen <bocean729s@gmail.com>
2026-02-14 11:45:06 +0800

    fix(overlay): 调整配置面板布局并增加宽度
    移除配置面板中的网格容器注释，使复选框标签垂直排列。同时将覆盖组件宽度从310px增加到350px，以容纳新的布局并改善可读性。

--- src/components/OverlayConfigPanel.vue
+++ src/components/OverlayConfigPanel.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="overlay-config" @mousedown.stop>
-    <div class="overlay-config-grid overlay-config-grid--3">
-      <label>
-        <input v-model="prefs.showCpu" type="checkbox" />
-        {{ t('overlay.showCpu') }}
-      </label>
-      <label>
-        <input v-model="prefs.showGpu" type="checkbox" />
-        {{ t('overlay.showGpu') }}
-      </label>
-      <label>
-        <input v-model="prefs.showMemory" type="checkbox" />
-        {{ t('overlay.showMemory') }}
-      </label>
-      <label>
-        <input v-model="prefs.showDisk" type="checkbox" />
-        {{ t('overlay.showDisk') }}
-      </label>
-      <label>
-        <input v-model="prefs.showDown" type="checkbox" />
-        {{ t('overlay.showDown') }}
-      </label>
-      <label>
-        <input v-model="prefs.showUp" type="checkbox" />
-        {{ t('overlay.showUp') }}
-      </label>
-    </div>
+    <!-- <div class="overlay-config-grid overlay-config-grid--3"> -->
+    <label>
+      <input v-model="prefs.showCpu" type="checkbox" />
+      {{ t('overlay.showCpu') }}
+    </label>
+    <label>
+      <input v-model="prefs.showGpu" type="checkbox" />
+      {{ t('overlay.showGpu') }}
+    </label>
+    <label>
+      <input v-model="prefs.showMemory" type="checkbox" />
+      {{ t('overlay.showMemory') }}
+    </label>
+    <label>
+      <input v-model="prefs.showDisk" type="checkbox" />
+      {{ t('overlay.showDisk') }}
+    </label>
+    <label>
+      <input v-model="prefs.showDown" type="checkbox" />
+      {{ t('overlay.showDown') }}
+    </label>
+    <label>
+      <input v-model="prefs.showUp" type="checkbox" />
+      {{ t('overlay.showUp') }}
+    </label>
+    <!-- </div> -->
     <label>
       <input v-model="prefs.showLatency" type="checkbox" />
       {{ t('overlay.showLatency') }}
